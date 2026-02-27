607205d54cfd6afa1b8d99653471651c23c257c9
David Aning Amponsah <amponsahaning@gmail.com>
2026-02-27 14:44:12 -0800

    Add files via upload

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from 'sillyname';
+import {randomSuperhero} from 'superheroes';
+
+var sillyname = generateName();
+
+console.log(`your sillyname is ${sillyname}.`);
+
+var superhero = randomSuperhero();
+
+console.log(`your superhero name is ${superhero}.`);
