2ef7a276179988765e93d19262da8478a7acdda8
David Aning Amponsah <amponsahaning@gmail.com>
2026-03-13 15:26:04 -0700

    Add files via upload

---
+++ react.js/javascript-expressions-in-jsx-practice-forked/src/index.js
@@ -0,0 +1,21 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "David Aning Amponsah";
+
+ReactDOM.render(
+  <div>
+    <h1>Created by {name}</h1>
+    <h2>Copyright {new Date().getFullYear()}</h2>
+  </div>,
+  document.getElementById("root")
+);
