a4cc9bd57d6fdac369a7bca9e51c2e69e8641726
David Aning Amponsah <amponsahaning@gmail.com>
2026-03-15 16:02:19 -0700

    Add files via upload

---
+++ react.js/keeper-app-part-1-starting-forked/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <note className="note">
+      <h1>My note</h1>
+      <p>welcome to my note.</p>
+    </note>
+  );
+}
+
+export default Note;
